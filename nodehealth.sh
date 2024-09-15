774b207bc8863b1095a4f397fb7a3a3d5ae02e76
vamsi <munagalavamsi37@gmail.com>
2024-09-15 18:37:28 +0530

    node health

---
+++ nodehealth.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+########################
+# Author : Vamsi
+# Date : 15/09/2024
+#
+# This script is about the node health
+#
+# Version : v1
+#######################
+
+df -h
+
+free -g
+
+nproc
